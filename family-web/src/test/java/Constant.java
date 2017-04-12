66ef8a07a166593c8ad6556716ae9df0179f5c49
liaopng@163.com <liaopng@163.com>
2017-04-13 01:01:51 +0800

    xx

--- family-web/src/test/java/Constant.java
+++ family-web/src/test/java/Constant.java
@@ -4,5 +4,5 @@ public class Constant {
 //	public static final String HOST = "http://localhost:8081";
 //	public static final String access_token = "eyJpIjoiMTI3LjAuMC4xIiwibCI6IjcwZWZkZjJlYzliMDg2MDc5Nzk1YzQ0MjYzNmI1NWZiIiwiciI6IjkyNzRlZGM1NTQ3NDQ2MDg5NGZkMjhkZWNhNGIyY2RhIiwidCI6MTQ5MTkyMjA5NSwidSI6MTd9";
 	public static final String HOST = "http://api.mcwh123.com";
-	public static final String access_token = "eyJpIjoiMjIzLjczLjIwNi4xMzIiLCJsIjoiNzBlZmRmMmVjOWIwODYwNzk3OTVjNDQyNjM2YjU1ZmIiLCJyIjoiODQ1NDRmOWI3NDhkNDYzNjgwNDYxMDNjMTA3ZjY2NTAiLCJ0IjoxNDkyMDEyMDYzLCJ1IjoxN30=";
+	public static final String access_token = "eyJpIjoiMjIzLjczLjIwNi4xMzIiLCJsIjoiNzBlZmRmMmVjOWIwODYwNzk3OTVjNDQyNjM2YjU1ZmIiLCJyIjoiZWJiNjIyYmNiOTM4NGY0ZWI3NjQxNGRmNmU4MWQ4MTIiLCJ0IjoxNDkyMDE2Mjc2LCJ1IjoxN30=";
 }
